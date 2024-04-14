f97ca4762305442b256b6d52a2147ceb1eda08ce
MarioHCezar <mariohcezar@gmail.com>
2024-04-14 19:33:23 -0300

    "retornando dados iniciais"

--- src/main/java/br/com/mario/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/mario/screenmatch/ScreenmatchApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.model.DadosSerie;
 import br.service.ConsumoAPI;
+import br.service.ConverteDados;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -13,12 +15,18 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	private String url = "http://www.omdbapi.com/?i=tt3896198&apikey=d73f361a";
+
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Primeiro projeto Spring sem web");
 		ConsumoAPI consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("http://www.omdbapi.com/?i=tt3896198&apikey=d73f361a");
-		System.out.println(json);
+		var json = consumoAPI.obterDados(url);
+		// System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 
 }
